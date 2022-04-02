ecda39c80be4ad9c4f3c7a8fd856813523e0be06
Vinay Gupta <vinay.dhyutidhara@gmail.com>
2022-04-03 02:19:56 +0530

    footer added

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+import styles from "./Footer.module.css";
+
+export default function Footer() {
+  return (
+    <div>
+      <div className={styles.footer__row}></div>
+      <section class={styles.interested}>
+        <p>Interested in hiring me?</p>
+        <a href="#">Click Me!</a>
+      </section>
+      <div className={styles.footer__row}></div>
+      {/* Social Media Starts */}
+      <section className={styles.social}>
+        {/* Email */}
+        <a
+          className={styles.socialBox}
+          target="_blank"
+          rel="noreferrer"
+          href="mailto:vinay.dhyutidhara.com"
+        >
+          <div className={styles.socialBoxItem}>
+            <p>Email me</p>
+          </div>
+        </a>
+        {/* GitHub */}
+        <a
+          className={styles.socialBox}
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.github.com/vdhyutidhara"
+        >
+          <div className={styles.socialBoxItem}>
+            <p>
+              Follow me <br /> on GitHub
+            </p>
+          </div>
+        </a>
+        {/* LinkedIn */}
+        <a
+          className={styles.socialBox}
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.linkedin.com/vdhyutidhara"
+        >
+          <div className={styles.socialBoxItem}>
+            <p>
+              Follow me <br /> on LinkedIn
+            </p>
+          </div>
+        </a>
+        {/* Twitter */}
+        <a
+          className={styles.socialBox}
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.twitter.com/vdhyutidhara"
+        >
+          <div className={styles.socialBoxItem}>
+            <p>
+              Follow me <br /> on Twitter
+            </p>
+          </div>
+        </a>
+        {/* Social Media Ends */}
+      </section>
+      {/* <section>
+        <a
+          className={styles.inspiration}
+          href="https://canalstreet.market/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Site Design Inspiration
+        </a>
+      </section> */}
+    </div>
+  );
+}
