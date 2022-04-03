8402b90d6c64494f544e0d38a3539262e61d1acf
Vinay Gupta <vinay.dhyutidhara@gmail.com>
2022-04-03 08:16:54 +0530

    basic template made

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,5 +2,15 @@ import React from "react";
 import styles from "./Header.module.css";
 
 export default function Header() {
-  return <div className={styles.header}>Header</div>;
+  return (
+    <div className={styles.header}>
+      <section>
+        <h1 className={styles.headerText}>
+          Hello, My name is <a href="#"> Vinay Gupta</a>. I am from Earth.
+          It's feel nice to see you here.
+          Scroll below to know something interesting about me.
+        </h1>
+      </section>
+    </div>
+  );
 }
--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,8 +1,25 @@
-import React from 'react';
-import styles from './Main.module.css';
+import React from "react";
+import styles from "./Main.module.css";
 
 export default function Main() {
   return (
-    <div className={styles.main}>Main</div>
-  )
+    <div className={styles.main}>
+      <section className={styles.mainHeading}>
+        <h3>Things I have made</h3>
+        <div>
+          <div className={styles.projectCard}>1</div>
+          <div className={styles.projectCard}>2</div>
+          <div className={styles.projectCard}>3</div>
+          <div className={styles.projectCard}>4</div>
+          <div className={styles.projectCard}>5</div>
+          <div className={styles.projectCard}>6</div>
+        </div>
+      </section>
+      {/* Technologies */}
+      <section className={styles.tech}>
+        <h3>Things I know</h3>
+        <div className={styles.headingHr}></div>
+      </section>
+    </div>
+  );
 }
