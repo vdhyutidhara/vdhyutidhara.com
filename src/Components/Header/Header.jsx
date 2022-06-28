151b50e2a10600d8e47e9c68ba1f52e729d76fb6
Vinay Gupta <Vinay.dhyutidhara@gmail.com>
2022-06-28 16:57:59 +0530

    28062022-HEADER: Added nyc map and my details and improve details

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,16 +4,17 @@ import styles from "./Header.module.css";
 export default function Header() {
   return (
     <div className={styles.header}>
-      <section>
-        <h1 className={styles.headerText}>
-          Hello, My name is <span> Vinay Gupta</span>.
-          <br />I am from Earth.
-          <br />
-          It's feel nice to see you here.
-          <br />
-          Please scroll below.
-        </h1>
-      </section>
+      <div className={styles.headerNycMap}>
+        <img className={styles.nycMap} src="/images/nyc-map.webp" alt="nyc-map" />
+      </div>
+      <div className={styles.headerText}>
+        <p>
+          Hi, I am Vinay Gupta,
+          <br />a Web Developer from Earth.
+          <br />Let me introduce myself.
+        </p>
+        <img src="/images/details.png" alt="my-info" />
+      </div>
     </div>
   );
 }
