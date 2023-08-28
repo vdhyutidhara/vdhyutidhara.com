7d7c228bb38a535a656f6bc1415ce231c69fb015
Vinay Gupta <Vinay.dhyutidhara@gmail.com>
2023-08-28 17:20:48 +0530

    Design Template for Dekstop View

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -16,11 +16,11 @@ export default function Main() {
       <section className={styles.tech}>
         <h3>Things I know</h3>
         <div className={styles.headingHr}>
-          <img src="https://img.icons8.com/?size=512&id=38388&format=png" alt="git-icon" />
-          <img src="https://img.icons8.com/?size=512&id=58811&format=png" alt="react-icon" />
           <img src="https://img.icons8.com/ios/256/html-5.png" alt="html5-icon" />
           <img src="https://img.icons8.com/ios/256/css3.png" alt="css3-icon" />
           <img src="https://img.icons8.com/ios/256/javascript.png" alt="js-icon" />
+          <img src="https://img.icons8.com/?size=512&id=38388&format=png" alt="git-icon" />
+          <img src="https://img.icons8.com/?size=512&id=58811&format=png" alt="react-icon" />
           <img src="https://img.icons8.com/?size=512&id=DYjckUr8cF8H&format=png" alt="sass-icon" />
           <img src="https://img.icons8.com/?size=512&id=vMqgHSToxrJR&format=png" alt="typescript-icon" />
           <img src="https://img.icons8.com/?size=512&id=m6GFODBlJETz&format=png" alt="netlify-icon" />
